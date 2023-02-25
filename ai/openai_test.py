b58a357430ab4f364e054b7c699b1c93b3c1e3ee
leoven4 <leonardo.ventura424@gmail.com>
2023-02-25 13:42:06 +0100

    AI test

---
+++ ai/openai_test.py
@@ -0,0 +1,16 @@
+import openai
+from constants import constants
+
+openai.api_key = constants["openai_api_key"]
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Who is Batman?",
+  temperature=0,
+  max_tokens=60,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
+
+print(response)
